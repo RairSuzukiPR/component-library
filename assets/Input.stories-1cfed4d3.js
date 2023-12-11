f8a9729507ebbe31cc9c9e0dff1f9a1bc5a162c2
Rair Suzuki <rairsuzukipr@gmail.com>
2023-12-11 09:24:32 -0300

    Updates

---
+++ assets/Input.stories-1cfed4d3.js
@@ -0,0 +1,8 @@
+import{j as p,a as l}from"./jsx-runtime-03b4ddbf.js";import{F as e}from"./Field-5ccf9f76.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./styles-3bf26d79.js";import"./polished.esm-021f8831.js";import"./index-8d47fad6.js";const F={title:"Example/Input",component:e.Input,subcomponents:{Field:e,label:e.Label}},r=({placeholder:n})=>p(e,{children:[l(e.Label,{children:"Hello"}),l(e.Input,{placeholder:n})]});r.args={placeholder:"Placeholder content"};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  placeholder
+}) => {
+  return <Field>
+      <Field.Label>Hello</Field.Label>
+      <Field.Input placeholder={placeholder} />
+    </Field>;
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const b=["Default"];export{r as Default,b as __namedExportsOrder,F as default};
